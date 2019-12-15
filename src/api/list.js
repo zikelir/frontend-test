a76c55ef13ddcfbd527914c2901faac4c3bf4d5c
zikelir <pvselano@gmail.com>
2019-12-15 18:16:58 -0300

    displaying data in front-end

--- src/api/list.js
+++ src/api/list.js
@@ -1,16 +1,12 @@
-export async function callMovieShort(movieName) {
-    try {
-        const resp = await fetch(`http://www.omdbapi.com/?t=${movieName}&plot=short&apikey=886580cf`)
-            .then(res => res.json())
+export function callMovieShort(movieName) {
+    const resp = fetch(`http://www.omdbapi.com/?t=${movieName}&plot=short&apikey=886580cf`)
+            .then((res) =>
+                res.json()
+            )
             .then((data) => {
-                return data
+                return data;
             })
             .catch(console.log, ' api error');
-
         return resp;
-    } catch(err) {
-        console.log(err);
-    }
-    
 };
 
--- src/components/moviesList/moviesList.js
+++ src/components/moviesList/moviesList.js
@@ -7,9 +7,11 @@ class MoviesList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            category: '',
             movie: '',
             actionArr: ['Deadpool', 'X-men', 'Dragon Ball'],
+            adventureArr: ['The mummy', 'The matrix'],
+            comedyArr: ['Best in Show', 'The Hat'],
+            fantasyArr: ['The lord of the rings', 'Jumanji'],
             movies: []
         }
       }
@@ -20,28 +22,64 @@ class MoviesList extends React.Component {
 
     getPathName() {
        const pathName = window.location.pathname.split('/movies/');
-       const path = pathName[1];
-       this.setState({category: path});
+       const path = pathName[1];  
+       const moviesArr = [];
+       var categoryArr = [];
+       
+       categoryArr = this.selectedCategory(path);
+       categoryArr.map((item, index) => {
+               this.getMovieData(item).then((data) => {
+                   console.log(data, ' get movies data');
+
+                   moviesArr.push(data);
+                   this.setState({movies: moviesArr})
+                   return moviesArr;
+               }).catch((error) => {
+                   console.log(error);
+               });
+
+               console.log(this.state.movies)
+           });
     }
 
     getMovieData(name) {
-        callMovieShort(name).then((data) => {
+        return callMovieShort(name).then((data) => {
             return data;
         }).catch((err) => {
             console.log(err)
         });  
     }
 
+    selectedCategory(category) {
+        console.log(category, 'CAT')
+        if(category === 'Action') {
+            return this.state.actionArr;
+        }
+
+        else if (category === 'Adventure') {
+            return this.state.adventureArr;
+        }
+
+        else if (category === 'Comedy') {
+            return this.state.comedyArr;
+        }
+
+        else if (category === 'Fantasy') {
+            return this.state.fantasyArr;
+        }
+    }
+
     render() {
+
         return (
             <div className="movies">
                 <Header></Header>
                 <div className="movies-list-container">
                     <div className="page-title">{this.state.category} Movies</div>
                     {
-                        this.state.actionArr.map((item, index) => {
-                            return <div>Movie {index} - {JSON.stringify(this.getMovieData(item))}</div>
-                        })
+                        this.state.movies.length !== 0 ? this.state.movies.map((item, index) => {
+                            return <div>Movie {index} - {JSON.stringify(this.state.movies[index])}</div>
+                        }) : <div>No movies </div>
                     }
                 </div>
             </div>
