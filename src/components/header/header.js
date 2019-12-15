aad4a03534033f610e9adbdc1416118c6ea73fe8
zikelir <pvselano@gmail.com>
2019-12-15 20:55:43 -0300

    started handle text

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,16 +2,31 @@ import React from 'react';
 import { Link } from "@reach/router";
 import './header.css';
 
-function Header() {
-  return (
-    <div className="header-bar">
-        <div className="search-area">
-          <input type="text" placeholder="search" className="search-bar"></input>
-          <button className="search-button">Search</button>
-        </div>
-        <Link to="/" className="back-button"> {`<`} Back</Link>
-    </div>
-  );
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        text: '',
+        
+    }
+  }
+
+  handleText(target) {
+    const element = target.value;
+    console.log(target);
+    // this.setState({text: element})
+  }
+  render() {
+    return (
+      <div className="header-bar">
+          <div className="search-area">
+            <input type="text" placeholder="search" className="search-bar" onChange={(event) => this.setState({ text: event.target.value})}></input>
+            <button className="search-button" onClick={() => {this.props.handleSearch()}}>Search</button>
+          </div>
+          <Link to="/" className="back-button"> {`<`} Back</Link>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- src/components/moviesList/moviesList.js
+++ src/components/moviesList/moviesList.js
@@ -3,6 +3,7 @@ import { callMovieShort } from '../../api/list';
 import Header from '../header/header';
 import MovieCard from '../movieCard/movieCard';
 import './movies-list.css';
+import SearchComponent from '../searchComponent/searchComponent';
 
 class MoviesList extends React.Component {
     constructor(props) {
@@ -16,30 +17,31 @@ class MoviesList extends React.Component {
             fantasyArr: ['The lord of the rings', 'Jumanji', 'Hobbit'],
             movies: [],
             colapseInfo: true,
+            search: false,
         }
-      }
+    }
 
     componentDidMount() {
         this.getPathName();
     }
 
     getPathName() {
-       const pathName = window.location.pathname.split('/movies/');
-       const path = pathName[1];  
-       this.setState({category: path})
-       const moviesArr = [];
-       var categoryArr = [];
-        
-       categoryArr = this.selectedCategory(path);
-       categoryArr.map((item, index) => {
-               this.getMovieData(item).then((data) => {
-                   moviesArr.push(data);
-                   this.setState({movies: moviesArr})
-                   return moviesArr;
-               }).catch((error) => {
-                   console.log(error);
-               });
-           });
+        const pathName = window.location.pathname.split('/movies/');
+        const path = pathName[1];
+        this.setState({ category: path });
+        const moviesArr = [];
+        var categoryArr = [];
+
+        categoryArr = this.selectedCategory(path);
+        categoryArr.map((item, index) => {
+            this.getMovieData(item).then((data) => {
+                moviesArr.push(data);
+                this.setState({ movies: moviesArr })
+                return moviesArr;
+            }).catch((error) => {
+                console.log(error);
+            });
+        });
     }
 
     getMovieData(name) {
@@ -47,11 +49,11 @@ class MoviesList extends React.Component {
             return data;
         }).catch((err) => {
             console.log(err)
-        });  
+        });
     }
 
     selectedCategory(category) {
-        if(category === 'Action') {
+        if (category === 'Action') {
             return this.state.actionArr;
         }
 
@@ -68,25 +70,37 @@ class MoviesList extends React.Component {
         }
     }
 
+    handleSearch() {
+        this.setState({ search: true });
+    }
+
     render() {
 
         return (
             <div className="movies">
-                <Header></Header>
+                <Header handleSearch={this.handleSearch}></Header>
                 <div className="movies-list-container">
                     <div className="page-title">Recommended {this.state.category} Movies</div>
-                    <div className="movies-cards">
-                        {
-                            this.state.movies.length !== 0 ? this.state.movies.map((item, index) => {
-                            return <div className="card-poster">
-                                        <MovieCard movie={item} category={this.state.category} index={index}></MovieCard> 
-                                    </div>
-                            }) : <div>No movies </div>
-                        }
-                    </div>
+                    {
+                        this.state.searching === 'true' ? (
+                            <SearchComponent></SearchComponent>
+                        ) : (
+                                <div className="movies-cards">
+                                    {
+                                        this.state.movies.length !== 0 ? this.state.movies.map((item, index) => {
+                                            return <div className="card-poster">
+                                                <MovieCard movie={item} category={this.state.category} index={index}></MovieCard>
+                                            </div>
+                                        }) : <div>No movies </div>
+                                    }
+                                </div>
+                            )
+                    }
+
+
                 </div>
             </div>
-          )
+        )
     }
 }
 
