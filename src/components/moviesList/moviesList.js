61af3bb6049af774230d2333cfbf413419ed2c4c
zikelir <pvselano@gmail.com>
2019-12-17 00:13:44 -0300

    Some refactor

--- src/components/moviesList/moviesList.js
+++ src/components/moviesList/moviesList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { callMovieShort } from '../../api/list';
-import Header from '../header/header';
+// import Header from '../header/header';
+import { Link } from "@reach/router";
 import MovieCard from '../movieCard/movieCard';
 import './movies-list.css';
+import './header.css';
 import SearchComponent from '../searchComponent/searchComponent';
 
 class MoviesList extends React.Component {
@@ -11,13 +13,12 @@ class MoviesList extends React.Component {
         this.state = {
             movie: '',
             category: '',
-            actionArr: ['Deadpool', 'X-men', 'Dragon Ball', 'Thor', 'Spider-man'],
-            adventureArr: ['The mummy', 'The matrix', 'Aquaman'],
-            comedyArr: ['Best in Show', 'The Hat', 'The Beach Bum'],
-            fantasyArr: ['The lord of the rings', 'Jumanji', 'Hobbit'],
+            categoryMoviesArr: [],
             movies: [],
             colapseInfo: true,
             search: false,
+            text: '',
+            filteredMovie: []
         }
     }
 
@@ -33,15 +34,28 @@ class MoviesList extends React.Component {
         var categoryArr = [];
 
         categoryArr = this.selectedCategory(path);
-        categoryArr.map((item, index) => {
-            this.getMovieData(item).then((data) => {
-                moviesArr.push(data);
-                this.setState({ movies: moviesArr })
-                return moviesArr;
-            }).catch((error) => {
-                console.log(error);
+        if(this.state.filteredMovie.length === 0) {
+            categoryArr.map((item, index) => {
+                this.getMovieData(item).then((data) => {
+                    moviesArr.push(data);
+                    this.setState({ movies: moviesArr })
+                    return moviesArr;
+                }).catch((error) => {
+                    console.log(error);
+                });
             });
-        });
+        } else {
+            this.state.filteredMovie.map((item, index) => {
+                this.getMovieData(item).then((data) => {
+                    moviesArr.push(data);
+                    this.setState({ movies: moviesArr })
+                    return moviesArr;
+                }).catch((error) => {
+                    console.log(error);
+                });
+            });
+        }
+        
     }
 
     getMovieData(name) {
@@ -54,31 +68,71 @@ class MoviesList extends React.Component {
 
     selectedCategory(category) {
         if (category === 'Action') {
-            return this.state.actionArr;
-        }
+            const actionArr = ['Deadpool', 'X-men', 'Dragon Ball', 'Thor', 'Spider-man'];
+            this.setState({ categoryMoviesArr: actionArr });
+            return actionArr;
+        }  
 
         else if (category === 'Adventure') {
-            return this.state.adventureArr;
+            const adventureArr = ['The mummy', 'The matrix', 'Aquaman'];
+            this.setState({ categoryMoviesArr: adventureArr });
+            return adventureArr;
         }
 
         else if (category === 'Comedy') {
-            return this.state.comedyArr;
+            const comedyArr = ['Best in Show', 'The Hat', 'The Beach Bum'];
+            this.setState({ categoryMoviesArr: comedyArr });
+            return comedyArr;
         }
 
         else if (category === 'Fantasy') {
-            return this.state.fantasyArr;
+            const fantasyArr = ['The lord of the rings', 'Jumanji', 'Hobbit'];
+            this.setState({ categoryMoviesArr: fantasyArr });
+            return fantasyArr;
         }
     }
 
-    handleSearch() {
-        this.setState({ search: true });
+    handleSearch(e) {
+        const searchString = e.target.value;
+        this.setState({text: searchString});
+        const filteredArr = this.state.categoryMoviesArr.filter((item) => {
+            return item === searchString;
+        });
+        this.setState({ filteredMovie: filteredArr });   
+        if(e.target.value.length === 0) {
+            this.getPathName();
+        }    
+
+        if (this.state.categoryMoviesArr === this.state.filteredMovie) {
+            console.log('1333');
+            this.submitSearch();
+        }
+    }
+
+    submitSearch() {
+        const {filteredMovie, categoryMoviesArr} = this.state;
+       const totalArray = categoryMoviesArr;
+       if(filteredMovie.length > 0) {
+           this.setState({categoryMoviesArr: filteredMovie});
+           console.log(filteredMovie, categoryMoviesArr);
+           this.getPathName();
+       } else {
+            this.setState({categoryMoviesArr: totalArray});
+            this.getPathName();
+       }
     }
 
     render() {
 
         return (
             <div className="movies">
-                <Header handleSearch={() => {this.handleSearch()}}></Header>
+                <div className="header-bar">
+                    <div className="search-area">
+                        <input type="text" placeholder="search" className="search-bar" onChange={(e) => this.handleSearch(e)}  value={this.state.text} ></input>
+                        <button className="search-button" onClick={() => {this.submitSearch()}}>Search</button>
+                    </div>
+                    <Link to="/" className="back-button"> {`<`} Back</Link>
+                </div>
                 <div className="movies-list-container">
                     <div className="page-title">Recommended {this.state.category} Movies</div>
                     {
@@ -89,7 +143,7 @@ class MoviesList extends React.Component {
                                     {
                                         this.state.movies.length !== 0 ? this.state.movies.map((item, index) => {
                                             return <div className="card-poster">
-                                                <MovieCard movie={item} category={this.state.category} index={index}></MovieCard>
+                                                <MovieCard movie={item} category={this.state.category} key={index}></MovieCard>
                                             </div>
                                         }) : <div>No movies </div>
                                     }
